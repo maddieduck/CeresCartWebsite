c5a7860d6b0ebc02be71052b848a4ed15f5a59f6
Maddie <maddieduck@gmail.com>
2025-04-10 20:28:54 -0500

    fixing new page from button

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // or wherever your React entry point is
+  entry: './src/index.js', // Make sure this file imports any .jsx you need
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader'
       },
@@ -21,22 +21,22 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       }
     ]
-  },  
+  },
   resolve: {
     extensions: ['.js', '.jsx']
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // where your HTML lives
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
     }),
     new CopyWebpackPlugin({
-        patterns: [
-            { from: './src/ReuseCode/images', to: 'images' },
-            //{ from: './html', to: 'html' },
-            { from: './src', to: 'src', globOptions: { ignore: ['**/*.js', '**/*.jsx'] } }, // Exclude compiled files
-            //{ from: './src/css*', to: 'styles.css' }, // Only copy, no minification
-        ],
-      }),
+      patterns: [
+        { from: './src/ReuseCode/images', to: 'images', noErrorOnMissing: true }
+        // Removed copying of src so that JSX gets compiled, not copied
+      ]
+    })
   ],
   watchOptions: {
     ignored: /node_modules/,
